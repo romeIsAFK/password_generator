d50874e017cf36dae4c8226541b846a35ac168ea
Jerome <57968673+romeIsAFK@users.noreply.github.com>
2022-09-30 21:39:19 -0700

    Update randompassword.py

--- randompassword.py
+++ randompassword.py
@@ -2,9 +2,25 @@
 import random
 import string
 
+# Variables
+menu_options = {
+    1: 'Generate passcode',
+    2: 'Generate pincode',
+    3: 'Exit'
+}
+
+# definitions and functions
+def print_menu():
+    for key in menu_options.keys():
+        print(key,'-', menu_options[key])
+
 # Program says hello!
-print(
-    'Hello! Welcome to Password Generator'
-)
-
-
+print('\nHello! Welcome to the Password Generator.\n')
+while (True):
+    option = 0
+    while 1 > option or 3 < option:
+        try:
+            print_menu()
+            option = int(input('\nEnter your choice:'))
+        except:
+            print('\nWrong input, try again.\n')
