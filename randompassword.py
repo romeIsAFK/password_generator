123524004a66f2e31b36abcb63df2e250d0f0939
Jerome <57968673+JromeGit@users.noreply.github.com>
2022-10-04 20:33:43 -0700

    Update randompassword.py

--- randompassword.py
+++ randompassword.py
@@ -1,10 +1,47 @@
 # Import modules
+from multiprocessing.sharedctypes import Value
 import random
 import string
 
 # Program says hello!
 print(
-    'Hello! Welcome to Password Generator'
+    '\nHello! Welcome to a Password Generator!\n'
 )
 
+# Define characters
+letters = string.ascii_lowercase
+digits = string.digits
+special_chars = string.punctuation
 
+# Other definitions
+# def 
+
+
+# Main
+def main():
+    options = {
+        "1":"Generate Pincode",
+        "2":"Generate Passcode",
+        "3":"Exit"
+    }
+    
+    choice = 0
+    while choice < 1 or choice > 3:
+        try:
+            for key in options:
+                print(f'{key}:{options[key]}')
+
+            choice = int(input("\nEnter your choice:"))
+
+            if choice == 1 or choice == 2:
+                break
+            elif choice == 3:
+                exit()
+            else:
+                print("\nInvalid input. Please enter an integer from 1 to 3\n")
+        except:
+                if choice > 3:
+                    print("\nInvalid input. Please enter an integer from 1 to 3\n")
+
+
+main()
